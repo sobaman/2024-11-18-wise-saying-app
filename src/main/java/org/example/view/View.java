285ffe1461afb0ff48f883b293f54c6b73b09af6
sobaman <sobaman522@gmail.com>
2024-11-18 16:56:59 +0900

    2단계 기능 구현 완료

--- src/main/java/org/example/view/View.java
+++ src/main/java/org/example/view/View.java
@@ -4,12 +4,30 @@ import java.util.Scanner;
 
 public class View {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void start() {
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
-        Scanner scanner = new Scanner(System.in);
-        if (scanner.next().equals("종료")) {
-            scanner.close();
+        boolean flag = true;
+
+        while (flag) {
+            System.out.print("명령) ");
+            flag = register(scanner.nextLine());
         }
+
+    }
+
+    private static boolean register(String input) {
+        if (input.equals("등록")) {
+            System.out.print("명언 : ");
+            scanner.nextLine();
+            System.out.print("작가 : ");
+            scanner.nextLine();
+            return true;
+        } else if (input.equals("종료")) {
+            scanner.close();
+            return false;
+        }
+        return true;
     }
 }
